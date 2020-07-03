7a9a1622ae717aeab12bfa02a635f7b89ed7ce3c
Apartkktrain <64818647+Apartkktrain@users.noreply.github.com>
2020-07-03 11:38:22 +0900

    configでメッセージを変更できるように

--- quitreason/src/apartkktrain/quitreason/Main.php
+++ quitreason/src/apartkktrain/quitreason/Main.php
@@ -22,6 +22,12 @@ class Main extends PluginBase implements Listener
         $this->getLogger()->notice("----------------------");
         $this->getServer()->getPluginManager()->registerEvents($this, $this);
 
+                $this->config = new Config($this->getDataFolder() . "quitmessage.yml", Config::YAML, array(
+            "通常退室" => "§6[§b退室§6]§a %name が自らの意思で退室しました。",
+            "タイムアウト" => "§6[§b退室§6]§a $%ame がタイムアウトにより退室しました。",
+            "サーバーエラー" => "§6[§b退室§6]§a %name がサーバーエラーにより退室しました。\n§4サーバー管理者又は開発者まで報告をお願いします。",
+            ));
+
     }
 
     public function onquit(PlayerQuitEvent $event)
@@ -29,8 +35,17 @@ class Main extends PluginBase implements Listener
     	$reason = $event->getQuitReason();
     	$player = $event->getPlayer();
     	$name   = $event->getPlayer()->getName();
+
+         ///取得
+        $quit1 = $this->config->get("通常退室") ;
+        $quit2 = $this->config->get("タイムアウト");
+        $quit3 = $this->config->get("サーバーエラー");
+        ///変数の置き換え
+        $quit1 = str_replace("%name", $name, $quit1);
+        $quit2 = str_replace("%name", $name, $quit2);
+        $quit3 = str_replace("%name", $name, $quit3);
     	if ($reason === 'client disconnect') {
-    		$event->setQuitMessage("§6[§b退室§6]§a $name が自らの意思で退室しました。");
+    		$event->setQuitMessage($quit1);
     		return true;
 
     	}
